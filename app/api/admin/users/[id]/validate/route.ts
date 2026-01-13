14e29469b0dcf9c7c21a4e9e121ad66ff67d4ae7
renatusmanoff <239621397+usmanoffcom@users.noreply.github.com>
2026-01-13 20:07:13 +0300

    fix: APPROVED -> COMPLETED in validate route

--- app/api/admin/users/[id]/validate/route.ts
+++ app/api/admin/users/[id]/validate/route.ts
@@ -66,7 +66,7 @@ export async function POST(
           },
         },
         data: {
-          status: "APPROVED",
+          status: "COMPLETED",
         },
       });
     } else if (status === "REJECTED") {
