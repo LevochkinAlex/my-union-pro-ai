196bca34412d4544aa132dd1b554a0f1b590f2c8
renatusmanoff <239621397+usmanoffcom@users.noreply.github.com>
2026-03-16 21:21:01 +0300

    Add Yandex.Metrika counter 107726457

--- components/AnalyticsScripts.tsx
+++ components/AnalyticsScripts.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { useEffect, useRef } from "react";
 
 const MAIL_RU_COUNTER_ID = "3749973";
+const YANDEX_METRIKA_ID = 107726457;
 const VK_PIXEL_ID = process.env.NEXT_PUBLIC_VK_PIXEL_ID;
 
 export function AnalyticsScripts() {
@@ -59,6 +60,28 @@ export function AnalyticsScripts() {
             style={{ position: "absolute", left: -9999 }}
             alt="Top.Mail.Ru"
           />
+        </div>
+      </noscript>
+
+      {/* Yandex.Metrika */}
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRIKA_ID}', 'ym');
+ym(${YANDEX_METRIKA_ID}, 'init', { ssr: true, webvisor: true, clickmap: true, ecommerce: 'dataLayer', referrer: document.referrer, url: location.href, accurateTrackBounce: true, trackLinks: true });
+          `.trim(),
+        }}
+      />
+      <noscript>
+        <div>
+          <img src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} style={{ position: "absolute", left: -9999 }} alt="" />
         </div>
       </noscript>
 
